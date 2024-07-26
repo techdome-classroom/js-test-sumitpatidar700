2ef3d3d51555184141646e99fba49af2c405bc5b
sumit <sumitpatidar700@gmail.com>
2024-07-26 12:17:48 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -28,24 +28,6 @@ const getTotalIsles = function (grid) {
     return islandCount;
 }
 
-// Example usage:
-const map1 = [
-    ["L","L","L","L","W"],
-    ["L","L","W","L","W"],
-    ["L","L","W","W","W"],
-    ["W","W","W","W","W"],
-];
 
-const map2 = [
-    ["L","L","W","W","W"],
-    ["L","L","W","W","W"],
-    ["W","W","L","W","W"],
-    ["W","W","W","L","L"],
-];
-
-console.log(numIslands(map1));  // Output: 1
-console.log(numIslands(map2));  // Output: 3
-
-};
 
 module.exports = getTotalIsles;
