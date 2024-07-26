f63c40cb33afa4e4d99f94ac8723d9f0f2e55270
sumit <sumitpatidar700@gmail.com>
2024-07-26 12:17:40 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,5 @@
 const getTotalIsles = function (grid) {
-
-  function numIslands(grid) {
-    if (grid.length === 0) return 0;
+  if (grid.length === 0) return 0;
 
     const rows = grid.length;
     const cols = grid[0].length;
