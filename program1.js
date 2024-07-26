6524a3cb0ee891477fcc440b3221ce1b0b9988f8
sumit <sumitpatidar700@gmail.com>
2024-07-26 13:25:12 +0530

    solved question

--- program1.js
+++ program1.js
@@ -1,8 +1,45 @@
-const getTotalIsles = function (grid) {
-   let gridlength=grid.length 
- if (gridlength==0) {
-    return 0;
- }
-
-}
-module.exports = getTotalIsles;
+const direction = [
+   [-1, 0],
+   [1, 0],
+   [0, -1],
+   [0, 1],
+ ];
+ 
+ const solve = function (grid, i, j) {
+   grid[i][j] = '$';
+ 
+   for (let k = 0; k < 4; k++) {
+     let new_i = i + direction[k][0];
+     let new_j = j + direction[k][1];
+ 
+     if (
+       new_i >= 0 &&
+       new_i < grid.length &&
+       new_j >= 0 &&
+       new_j < grid[0].length &&
+       grid[new_i][new_j] == 'L'
+     ) {
+       solve(grid, new_i, new_j);
+     }
+   }
+ };
+ 
+ const getTotalIsles = function (grid) {
+   // write your code here
+   let m = grid.length;
+   let n = grid[0].length;
+ 
+   let count = 0;
+   for (let i = 0; i < m; i++) {
+     for (let j = 0; j < n; j++) {
+       if (grid[i][j] == "L") {
+         count++;
+         solve(grid, i, j);
+       }
+     }
+   }
+ 
+   return count;
+ };
+ 
+ module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,49 @@
 const decodeTheRing = function (s, p) {
 
-    // write your code here
 
-  };
+  let msgLen = s.length;
+  let keyLen = p.length;
+  let i = 0;
+  let j = 0;
+  let starIdx = -1;
+  let match = 0;
+
+  while (i < msgLen) {
+
+    if (j < keyLen && (p[j] === "?" || p[j] === s[i])) {
+      i++;
+      j++;
+    }
+
+
+    else if (j < keyLen && p[j] === "*") {
+      starIdx = j;
+      match = i;
+      j++;
+    }
+
   
-  module.exports = decodeTheRing;
+    else if (starIdx != -1) {
+      j = starIdx + 1;
+      match++;
+      i = match;
+    }
+
+  
+    else {
+      return false;
+    }
+  }
+
+
+
+  if (j < keyLen && p[j] === "*") {
+    return j++;
+  }
+
+  if (j === keyLen) return true;
+  else return false;
+};
+
+
+module.exports = decodeTheRing;
